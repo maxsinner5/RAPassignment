0fa4a09a8747731fece0087cee58e69abd5c211b
maxsinner5 <maxsinner@yahoo.de>
2023-01-09 10:46:00 +0100

    Tested package successfully
    d

--- tests/testthat/test-RAPassignment.R
+++ tests/testthat/test-RAPassignment.R
@@ -1,16 +1,21 @@
 
 
-exo_planets <- readr::read_csv("https://raw.githubusercontent.com/maxsinner5/RAPassignment/main/all_exoplanets_2021.csv")
+exo_planets <- readr::read_csv("all_exoplanets_2021.csv")
+
 test_that("Function returns a tibble that is nx5",{
 
 
 
   returned_tibble <- exo_planets_data(exo_planets,
-                                      exo_planets$Distance,
-                                      exo_planets$`Discovery Year`)
+                                      num_stars,
+                                      discovery_method)
 
 
-  dim(returned_tibble)[2] = 5
+  returned_value <- dim(returned_tibble)[2]
+
+  expected_value <- 5
+
+  expect_equal(returned_value,expected_value)
 
 
 
